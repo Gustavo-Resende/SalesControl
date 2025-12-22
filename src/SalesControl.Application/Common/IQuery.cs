e51ba562ed36fc5a5746d85f5ddfd5dfc269322a
Gustavo Resende <guguinharesende@hotmail.com>
2025-12-21 22:36:38 -0300

    Adiciona MediatR, padrões CQRS e AssemblyMarker
    Inclui referências aos pacotes MediatR, Ardalis.Result e Ardalis.Specification. Cria a classe AssemblyMarker para identificação do assembly. Adiciona interfaces ICommand/IQuery e seus handlers para padronizar comandos e queries usando MediatR no namespace

---
+++ src/SalesControl.Application/Common/IQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace SalesControl.Application.Common;
+
+public interface IQuery<out TResponse> : IRequest<TResponse> { }
+
+public interface IQueryHandler<in TQuery, TResponse>
+    : IRequestHandler<TQuery, TResponse>
+    where TQuery : IQuery<TResponse>
+{ }
