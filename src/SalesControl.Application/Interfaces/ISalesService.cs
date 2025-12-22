b3963f9cbf7cca51343e2d8f270cb5ad198b1a8c
Gustavo Resende <guguinharesende@hotmail.com>
2025-12-21 22:19:36 -0300

    Adicionada a interface interna ISalesService em ISalesService.cs, incluindo o método assíncrono RegisterSaleAsync para registro de vendas.

---
+++ src/SalesControl.Application/Interfaces/ISalesService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SalesControl.Application.Interfaces
+{
+    internal interface ISalesService
+    {
+        Task<Guid> RegisterSaleAsync(RegisterSaleDto dto, CancellationToken cancellationToken = default);
+    }
+}
